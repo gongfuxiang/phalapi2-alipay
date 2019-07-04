811ca3f601c989796e142f614975d36400e14246
devil_gong <devil_gong@wochacha.com>
2019-07-04 15:14:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# 支付宝支付
+
+支付宝支付基于支付宝api2.0接口实现 http://gong.gg/
+
+
+## 安装和配置
+修改项目下的composer.json文件，并添加：  
+```
+    "phalapi/alipay":"dev-master"
+```
+然后执行```composer update```。  
+
+## 注册
+在/path/to/phalapi/config/di.php文件中，注册：  
+```php
+$di->alipay = function() {
+    return new \PhalApi\Alipay\Lite();
+};
+```
+
+## 使用
+第一种使用方式：直接输出二维码图片：
+```php
+\PhalApi\DI()->alipay->Pay();
+```
---
+++ src/Lite.php
@@ -0,0 +1,27 @@
+<?php
+namespace PhalApi\Alipay;
+
+/**
+ * 支付宝支付
+ * @author  Devil
+ * @blog    http://gong.gg/
+ * @version 1.0.0
+ * @date    2019-07-04
+ * @desc    description
+ */
+class Lite {
+
+    /**
+     * @author  Devil
+     * @blog    http://gong.gg/
+     * @version 1.0.0
+     * @date    2019-07-04
+     * @desc    description
+     * @return  [type]          [description]
+     */
+    public function Pay()
+    {
+        return 'hello';
+    } 
+}
+?>
